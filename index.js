62752997d3ce3fb567fd3bb861cf2da624abdc68
MaldoNote <crismaldoalf@gmail.com>
2023-09-11 16:47:41 -0300

    jajdsjf

---
+++ index.js
@@ -0,0 +1,16 @@
+// let a = 15;
+
+// a/=3;
+// console.log(a)
+
+function name1 (){
+    return function name(...arg) {  
+        return "hola mundo"    
+    }
+}
+
+const hola= name1();
+
+console.log(hola('jhfgfghjm'));
+
+
