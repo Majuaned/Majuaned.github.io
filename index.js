2931a0edb99d3d59af9e6ff62ff35ee00e953c67
MaldoNote <crismaldoalf@gmail.com>
2023-09-13 19:13:37 -0300

    segundo comit

--- index.js
+++ index.js
@@ -3,14 +3,24 @@
 // a/=3;
 // console.log(a)
 
-function name1 (){
-    return function name(...arg) {  
-        return "hola mundo"    
-    }
-}
+// function name1 (){
+//     return function name(...arg) {  
+//         return "hola mundo"    
+//     }
+// }
 
-const hola= name1();
+// const hola= name1();
 
-console.log(hola('jhfgfghjm'));
+// console.log(hola('jhfgfghjm'));
+
+
+
+
+
+
+
+
+
+
 
 
